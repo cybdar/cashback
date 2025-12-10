da10482db14b0fb889452294a2dbf09617a3ec5b
cybdar <kari4ka21@gmail.com>
2025-12-11 00:24:41 +0200

    TestNG failing test

--- src/main/java/ru/netology/cashback/CashbackHacker.java
+++ src/main/java/ru/netology/cashback/CashbackHacker.java
@@ -4,12 +4,6 @@ public class CashbackHacker {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        int remainder = amount % boundary;
-
-        if (remainder == 0) {
-            return 0;
-        }
-
-        return boundary - remainder;
+        return boundary - amount % boundary;
     }
 }
--- src/test/java/ru/netology/cashback/CashbackHackerTest.java
+++ src/test/java/ru/netology/cashback/CashbackHackerTest.java
@@ -6,32 +6,10 @@ import org.testng.annotations.Test;
 public class CashbackHackerTest {
 
     @Test
-    public void shouldReturn0If1000() {
+    public void shouldFailWhenAmountIs1000() {
         CashbackHacker service = new CashbackHacker();
-
         int actual = service.remain(1000);
         int expected = 0;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    public void shouldReturn500If1500() {
-        CashbackHacker service = new CashbackHacker();
-
-        int actual = service.remain(1500);
-        int expected = 500;
-
-        Assert.assertEquals(actual, expected);
-    }
-
-    @Test
-    public void shouldReturn0If0() {
-        CashbackHacker service = new CashbackHacker();
-
-        int actual = service.remain(0);
-        int expected = 0;
-
         Assert.assertEquals(actual, expected);
     }
 }
