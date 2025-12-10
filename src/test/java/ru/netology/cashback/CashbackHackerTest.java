bfa93cac0c1ff924c161206e0808c78567faaed9
cybdar <kari4ka21@gmail.com>
2025-12-11 00:32:06 +0200

    JUnit4 failing test

--- src/test/java/ru/netology/cashback/CashbackHackerTest.java
+++ src/test/java/ru/netology/cashback/CashbackHackerTest.java
@@ -6,32 +6,10 @@ import org.junit.Assert;
 public class CashbackHackerTest {
 
     @Test
-    public void shouldReturn0If1000() {
+    public void shouldFailWhenAmountIs1000() {
         CashbackHacker service = new CashbackHacker();
-
         int actual = service.remain(1000);
         int expected = 0;
-
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldReturn500If1500() {
-        CashbackHacker service = new CashbackHacker();
-
-        int actual = service.remain(1500);
-        int expected = 500;
-
-        Assert.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldReturn0If0() {
-        CashbackHacker service = new CashbackHacker();
-
-        int actual = service.remain(0);
-        int expected = 0;
-
         Assert.assertEquals(expected, actual);
     }
 }
